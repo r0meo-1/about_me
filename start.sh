5fa878ce2cb118d8bbe3d6e97e67a668ddbd2678
r0meo-1 <r0meo1.ru@gmail.com>
2025-06-05 18:13:54 +0300

    Удалены лишние файлы supervisor и start.sh для Railway

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python3 /app/currency_converter/manage.py migrate --noinput
-python3 /app/currency_converter/manage.py collectstatic --noinput
-exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
